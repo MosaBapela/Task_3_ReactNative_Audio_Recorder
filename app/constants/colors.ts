b962f9fd714b3bb18ed122622f877714824404e6
MosaBapela <charitybapela2003@gmail.com>
2025-11-24 15:54:45 +0200

    added constants that defining the constant color pallete of the app

---
+++ app/constants/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: '#001B48',
+  secondary: '#02457A',
+  tertiary: '#018ABE',
+  light: '#97CADB',
+  background: '#D6E8EE',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#8E8E93',
+  error: '#FF3B30',
+  success: '#34C759',
+};
